7f03c1dcae94679b44b4d10eb76dbbb2267e202b
itisthenikhilsingh <113625518+itisthenikhilsingh@users.noreply.github.com>
2025-08-17 13:53:33 +0530

    Time: 16 ms (7.60%) | Memory: 25.9 MB (5.11%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& inte) {
-        vector<vector<int>> ans;
-        sort(inte.begin(),inte.end());
-        int k=0;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
-        for(int i=0;i<inte.size();i++)
+        if(intervals.size()<2) return intervals;
+
+        vector<vector<int>> ans;
+
+        sort(intervals.begin(),intervals.end());
+        
+        for(int i=0;i<intervals.size();i++)
         {
-            if(inte[k][1]>=inte[i][0])
+            if(ans.empty())
+            ans.push_back(intervals[i]);
+
+            vector<int> lastInterval=ans.back();
+            vector<int> currInterval=intervals[i];
+
+            if(lastInterval[1]>=currInterval[0])//operlaping
             {
-                inte[k][1] = max(inte[k][1],inte[i][1]);
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            else {
-                k++;
-                inte[k]=inte[i];
-            }
+            else ans.push_back(intervals[i]);
+
 
         }
-         inte.resize(k+1);
-         return inte;
+
+        return ans;
         
     }
 };
