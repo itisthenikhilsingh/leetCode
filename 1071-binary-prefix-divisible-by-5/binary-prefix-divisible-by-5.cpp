fa2fcf1e2418e9af59b2afe41201436bdc63ba80
Nikhil Singh <113625518+itisthenikhilsingh@users.noreply.github.com>
2025-11-24 22:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (37.78%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+
+        vector<bool> ans;
+        int val=0;
+
+        for( auto bit : nums)
+        {
+            val= (val*2+bit)%5;
+            ans.push_back(val==0);
+        }
+        return ans;
+        
+    }
+};
