2f6c3ded9e2aef640b22d37bd1426eace21143f9
Nikhil Singh <113625518+itisthenikhilsingh@users.noreply.github.com>
2025-12-23 01:11:03 +0530

    Time: 3 ms (40.97%) | Memory: 19.5 MB (63.81%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -2,10 +2,18 @@ class Solution {
 public:
     void dfs(int node ,vector<vector<int>>& isConnected, vector<int>& isVisted)
     {
+        //base conditon that if the node is alredy visited then just skip the part
+
+        //lopping through the all the connection of the node ini 2d array
         for(int i=0;i<isConnected.size();i++)
         {
+            //if we find the connection and that is not visted 
             if(isConnected[node][i]==1 && !isVisted[i]) {
+                
+                // the array is updated as we had alredy visited it
                 isVisted[i]=1;
+
+                // now visting all connection of the connected node 
                 dfs(i , isConnected ,isVisted);
                 }
         }
